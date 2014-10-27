368792867f14e7dcae6dc35cd627bbb2845a4cd7
fadimezhan <fadime_ozhan@hotmail.com>
2014-10-27 18:36:17 +0200

    Kare fonksiyonunu ekledim

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
 #include "stdio.h"
 
 int main() {
+	int sayi;
+	printf("Bir sayi girin:\n"); scanf("%d",&sayi);
+	kare(sayi);
+	
 	return 0;
+}
+int kare(int a)
+{
+	int sonuc;
+	sonuc=a*a;
+	printf("sonuc:%d\n",sonuc);
+	return sonuc;
+
 }
