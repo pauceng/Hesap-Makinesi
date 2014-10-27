847ee735f92308a5bffe4396c0d4b14ea348cc1b
sezercetin <acetin13@posta.pau.edu.tr>
2014-10-27 19:08:27 +0200

    Mod

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+int mod(int sayi1, int sayi2)
+{
+	return sayi1%sayi2;
+}
+int main()
+{
+	int a, b;
+	printf("Modu alinacak sayiyi giriniz:");
+	scanf_s("%d", &a);
+	printf("Modu giriniz:");
+	scanf_s("%d", &b);
+	printf("Sonuc:%d\n", mod(a, b));
 
-int main() {
-	return 0;
+	getch();
 }
