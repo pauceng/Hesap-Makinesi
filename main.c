cd5dd4255a433c01dec2106822bf6fd2125eb947
tarslan95 <isimsizbiradres@hotmail.com>
2014-10-27 19:04:18 +0200

    EBOB

--- main.c
+++ main.c
@@ -1,5 +1,42 @@
-#include "stdio.h"
+#include<stdio.h>
+#include<conio.h>
 
-int main() {
-	return 0;
+int EBOB(int sayi1,int sayi2)
+{
+	int kucuk,ebob,i;
+
+	if(sayi1>sayi2)
+	{
+		kucuk=sayi1;
+	}
+	else
+	{
+		kucuk=sayi2;
+	}
+
+	for(i=kucuk;i>=1;i--)
+	{
+		if((sayi1%i == 0)&&(sayi2%i == 0))
+		{
+			ebob=i;
+			break;
+		}
+		else
+		{
+			continue;
+		}
+	}
+	return ebob;
+}
+
+int main()
+{
+	int a,b;
+	scanf("%d",&a);
+	scanf("%d",&b);
+
+	printf("EBOB(%d,%d)=%d",a,b,EBOB(a,b));
+
+
+getch();
 }
