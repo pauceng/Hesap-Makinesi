894d82accbca9343d6f5ad5957c9762357622be7
irrkaya <irem_95@windowslive.com>
2014-10-27 18:59:45 +0200

    toplama

--- main.c
+++ main.c
@@ -1,5 +1,24 @@
-#include "stdio.h"
-
+#include <stdio.h>
+#include <conio.h>
+int topla(int,int);
 int main() {
-	return 0;
+
+	int sayi1=0;
+	int sayi2=0;
+	
+	printf("ilk sayiyi giriniz: ", sayi1);	
+	scanf("%d", &sayi1);
+
+	printf("ikinci sayiyi giriniz: ", sayi2);
+	scanf("%d", &sayi2);
+	int c = topla(sayi1,sayi2);
+	printf("%d",c);
+	scanf("%d",&sayi1);
+return 0;
+}
+
+int topla(int a, int b) {
+
+
+return a+b;
 }
