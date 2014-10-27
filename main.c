59ee13ba8293d5b5d6d499b6ca9bfcf9225b162c
Brigade45 <taklit93@yandex.com>
2014-10-27 20:28:46 +0200

    Faktoriyel hesap v2
    Güncelleme

--- main.c
+++ main.c
@@ -1,5 +1,20 @@
 #include "stdio.h"
-
-int main() {
+#include <conio.h>
+void FaktHesap()
+{
+	int i, a, fakt;
+	fakt = 1;
+	printf("Faktoriyeli alinacak sayiyi giriniz = ");
+	scanf_s("%d", &a);
+	for (i = 1; i <= a; i++)
+		{
+		fakt = fakt *i;
+		}
+	printf("%d! = %d", a ,fakt);
+}
+	int main() 
+{
+	FaktHesap();
+	_getch();
 	return 0;
 }
