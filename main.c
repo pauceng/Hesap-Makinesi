c2f2d9fbb41cec74ef6fdf69df8bdba95041e080
ecuner <ecuner@outlook.com>
2014-10-28 14:09:34 +0200

    bitir

--- main.c
+++ main.c
@@ -1,27 +1,38 @@
 #include <stdio.h>
-#include <conio.h>
 
-void ekok(int sayilar[])
+void main()
 {
-	int a = sayilar[0];
-	for(int i=0; i<(sizeof(sayilar) + 1); i++)
+	int sayilar[3];
+	sayilar[0] = 3;
+	sayilar[1] = 6;
+	sayilar[2] = 9;
+	printf("SONUC: %d",ekok(sayilar));
+}
+
+int ekok(int sayilar[])
+{
+	int a = sayilar[0], i,b,s = sizeof(sayilar)/sizeof(a);
+	for(i=0; i<s; i++)
 	{
-		if(i<a)
+		if(sayilar[i]<a)
 		{
 			a = sayilar[i];
 		}
 	}
-	for(int i=0; i<(sizeof(sayilar) + 1); i++)
+	for(i=2; i<(a+1); i++)
 	{
-		for(; a>0; a--)
+		for(b=0; b<s; b++)
 		{
-			if(sayilar[i] % a == 0)
+			if(sayilar[b] % i == 0)
 			{
-
+				if(b == s - 1)
+				{
+					return i;
+				}
 			}
 			else
 			{
-				continue;
+				break;
 			}
 		}
 	}
