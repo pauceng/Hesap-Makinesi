f9114212e8d90bfa8604443ac17482caba1d37ce
Murat Saygılı <muratsaygili1@gmail.com>
2014-11-02 01:46:38 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,32 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <conio.h>
 
-int main() {
-	return 0;
-}
-int xUssuY(int a,int b)
+int a = 0, b = 0;
+
+int xUssuY(int a, int b)
 {
-	int sonuc=a;
-	for(int i=1;i<b;i++)
+	int sonuc = a;
+	for (int i = 1; i<b; i++)
 	{
-		sonuc=sonuc*a;
+		sonuc = sonuc*a;
 	}
 
 	return sonuc;
+}
+void input()
+{
+	
+	printf("Kuvveti alinacak sayiyi giriniz:");
+	scanf_s("%d", &a);
+	printf("Kacinci kuvveti olacagini giriniz:");
+	scanf_s("%d", &b);
+
+}
+
+void main()
+{
+	input();
+	printf("%d uzeri %d ==> %d",a,b,xUssuY(a, b));
+
+	_getch();
 }
