cc8deb0f6cc44b3e8b87eb13348d6b07eccf7663
sezercetin <acetin13@posta.pau.edu.tr>
2014-11-01 18:31:27 +0200

    Dediklerini yapmaya çalıstım.Olmus mu?

--- main.c
+++ main.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 #include <conio.h>
-int mod(int sayi1, int sayi2)
-{
-	return sayi1%sayi2;
-}
+#include <stdlib.h>
+
+int mod(int sayi1, int sayi2);
+void input();
+
 int main()
+{
+	input();
+	return 0;
+}
+
+void input()
 {
 	int a, b;
 	printf("Modu alinacak sayiyi giriniz:");
@@ -13,6 +20,11 @@ int main()
 	printf("Modu giriniz:");
 	scanf_s("%d", &b);
 	printf("Sonuc:%d\n", mod(a, b));
-
 	getch();
+
+}
+
+int mod(int sayi1, int sayi2)
+{
+	return sayi1%sayi2;
 }
