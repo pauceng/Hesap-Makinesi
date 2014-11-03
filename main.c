0a63affbddb5e50a52a4cff79567fe400a4bf3a1
Sefa Erdogan <serdogan132@gmail.com>
2014-11-03 18:10:36 +0200

    Update main.c
    Denilen işlemleri yapmaya çalıştım :)

--- main.c
+++ main.c
@@ -1,16 +1,22 @@
 #include "stdio.h"
 #include <conio.h>
 #include <math.h>
+void log();
+void input();
 void log()
 {
 	float x;
 	double sonuc;
-	printf("Logaritmasini istedigin sayi?...: ");
-	scanf("%f",&x);
 	sonuc=log(x);
 	printf("Logaritma %f = %f",x,sonuc);
 }
+void input()
+{
+	printf("Logaritmasini istedigin sayi?...: ");
+	scanf("%f",&x);
+}
 int main() {
 	log();
+	input();
 	return 0;
 }
